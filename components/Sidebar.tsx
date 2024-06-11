9ba28ffe34f3649377ed3f2a98ddddc4fa4564db
ermiastsegu <ermiastsegu@yahoo.com>
2024-06-11 16:34:43 +0300

    sidebar completed.

---
+++ components/Sidebar.tsx
@@ -0,0 +1,68 @@
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
+import {
+  LayoutDashboard,
+  Newspaper,
+  Folders,
+  CreditCard,
+  Settings,
+  User,
+} from "lucide-react";
+import Link from "next/link";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <Command className="bg-secondary rounded-none">
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>
+              <LayoutDashboard className="mr-2 h-4 w-4" />
+              <Link href="/dashboard">Dashboard</Link>
+            </CommandItem>
+            <CommandItem>
+              <Newspaper className="mr-2 h-4 w-4" />
+              <Link href="#">Post</Link>
+            </CommandItem>
+            <CommandItem>
+              <Folders className="mr-2 h-4 w-4" />
+              <Link href="#">Categories</Link>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <LayoutDashboard className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <CreditCard className="mr-2 h-4 w-4" />
+              <span>Billing</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </div>
+  );
+};
+
+export default Sidebar;
