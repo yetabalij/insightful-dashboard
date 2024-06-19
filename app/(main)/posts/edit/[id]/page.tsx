c9a8c832960801a04cedcde3c4b22fd46454d656
ermiastsegu <ermiastsegu@yahoo.com>
2024-06-19 10:45:03 +0300

    edit post created.

---
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+import BackButton from "@/components/BackButton";
+
+const PostEditPage = () => {
+  return (
+    <div>
+      <BackButton text="Back To Posts" link="/posts" />
+    </div>
+  );
+};
+
+export default PostEditPage;
