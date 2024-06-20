c81bdcd1f0e0a60b1fdde230603f1182c813bc37
ermiastsegu <ermiastsegu@yahoo.com>
2024-06-20 10:24:27 +0300

    dark mode applied.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <div>{children}</div>
-        <Toaster />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          // enableSystem
+          // disableTransitionOnChange
+        >
+          <Navbar />
+          <div>{children}</div>
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import React from "react";
 import Logo from "@/img/insightfulwords1.png";
 import Link from "next/link";
 import Image from "next/image";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
 import {
   DropdownMenu,
@@ -13,8 +17,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
+  const { setTheme } = useTheme();
   return (
     <div className="bg-primary dark:bg-slate-700 py-2 px-5 flex justify-between text-white">
       {/* Logo */}
@@ -40,6 +46,27 @@ const Navbar = () => {
           </DropdownMenuItem>
           <DropdownMenuItem>
             <Link href="/auth">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {/* Dark mode  */}
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            Light
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
+            Dark
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>
+            System
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
